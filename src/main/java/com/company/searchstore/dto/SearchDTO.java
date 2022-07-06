d92e0d2c9f55838c7c6f2ba796877110be93d4f2
andre.coelho <coelho@hotmart.com>
2022-07-06 20:11:49 -0300

    fix max size

--- src/main/java/com/company/searchstore/dto/SearchDTO.java
+++ src/main/java/com/company/searchstore/dto/SearchDTO.java
@@ -3,6 +3,7 @@ package com.company.searchstore.dto;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -20,7 +21,7 @@ public class SearchDTO {
   private String text;
 
   @Min(10)
-  @Min(20)
+  @Max(20)
   private int size = 15;
 
   private Object searchAfterScore;
